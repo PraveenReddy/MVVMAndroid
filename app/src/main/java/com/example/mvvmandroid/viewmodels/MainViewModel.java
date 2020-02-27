2d0bc8f506dbf153a167a8df94b7ae25216ad130
Praveen <reddy2praveen@gmail.com>
2020-02-27 22:25:56 +0530

    commiting app code

---
+++ app/src/main/java/com/example/mvvmandroid/viewmodels/MainViewModel.java
@@ -0,0 +1,62 @@
+package com.example.mvvmandroid.viewmodels;
+
+import android.os.AsyncTask;
+
+import com.example.mvvmandroid.models.NicePlace;
+import com.example.mvvmandroid.repositories.NicePlaceRepository;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+
+    private MutableLiveData<List<NicePlace>> mNicePlaces;
+    private NicePlaceRepository mRepo;
+    private MutableLiveData<Boolean> mIsUpdating = new MutableLiveData<>();
+
+    public void init(){
+        if(mNicePlaces != null){
+            return;
+        }
+        mRepo = NicePlaceRepository.getInstance();
+        mNicePlaces = mRepo.getNicePlaces();
+    }
+
+    public void addNewValue(final NicePlace nicePlace){
+        mIsUpdating.setValue(true);
+
+        new AsyncTask<Void, Void, Void>(){
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                List<NicePlace> currentPlaces = mNicePlaces.getValue();
+                currentPlaces.add(nicePlace);
+                mNicePlaces.postValue(currentPlaces);
+                mIsUpdating.postValue(false);
+            }
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute();
+    }
+
+    public LiveData<List<NicePlace>> getNicePlaces(){
+        return mNicePlaces;
+    }
+
+    public LiveData<Boolean> getIsUpdating(){
+        return mIsUpdating;
+    }
+
+}
---
+++ app/src/main/java/com/example/mvvmandroid/views/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.mvvmandroid.views;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.example.mvvmandroid.R;
+import com.example.mvvmandroid.adapters.RecyclerAdapter;
+import com.example.mvvmandroid.models.NicePlace;
+import com.example.mvvmandroid.viewmodels.MainViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private FloatingActionButton mFab;
+    private RecyclerView mRecyclerView;
+    private RecyclerAdapter mAdapter;
+    private ProgressBar mProgressBar;
+    private MainViewModel mMainViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFab = findViewById(R.id.fab);
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mProgressBar = findViewById(R.id.progress_bar);
+
+        mMainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        mMainViewModel.init();
+
+        mMainViewModel.getNicePlaces().observe(this, new Observer<List<NicePlace>>() {
+            @Override
+            public void onChanged(@Nullable List<NicePlace> nicePlaces) {
+                mAdapter.notifyDataSetChanged();
+            }
+        });
+
+        mMainViewModel.getIsUpdating().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(@Nullable Boolean aBoolean) {
+                if(aBoolean){
+                    showProgressBar();
+                }
+                else{
+                    hideProgressBar();
+                    mRecyclerView.smoothScrollToPosition(mMainViewModel.getNicePlaces().getValue().size()-1);
+                }
+            }
+        });
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMainViewModel.addNewValue(
+                        new NicePlace(
+                                "https://i.imgur.com/ZcLLrkY.jpg",
+                                "Washington"
+                        )
+                );
+            }
+        });
+
+        initRecyclerView();
+    }
+    private void initRecyclerView(){
+        mAdapter = new RecyclerAdapter(this, mMainViewModel.getNicePlaces().getValue());
+        RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private void showProgressBar(){
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar(){
+        mProgressBar.setVisibility(View.GONE);
+    }
+}
